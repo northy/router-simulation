75cbca4f8cd863b46875837a0b73efe4f4590d9a
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-03-21 14:53:00 -0300

    Error in message structure comment

--- include/structures.h
+++ include/structures.h
@@ -2,7 +2,7 @@
 
 //received message
 typedef struct {
-    char type; //data = control; 1 = control
+    char type; //0 = data; 1 = control
     char source_router_ip[16];
     unsigned short source_router_port;
     char destination_router_ip[16];
