d98a77bb4cd86cc2beb087da9e58628dd0a6502e
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-03-24 18:24:36 -0300

    Changes to message structure's requisites

--- include/structures.h
+++ include/structures.h
@@ -3,10 +3,8 @@
 //received message
 typedef struct {
     char type; //0 = data; 1 = control
-    char source_router_ip[16];
-    unsigned short source_router_port;
-    char destination_router_ip[16];
-    unsigned short destination_router_port;
+    int source_router_id;
+    int destination_router_id;
     char payload[BUF_LEN];
 
 } r_message;
